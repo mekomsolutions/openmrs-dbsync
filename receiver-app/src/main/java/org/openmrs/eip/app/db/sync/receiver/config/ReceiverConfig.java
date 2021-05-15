c957e795c2dd61e6f902c9c5535e2b223b12739c
wluyima <wyclif@openmrs.org>
2021-05-15 12:48:57 -0500

    EIP-12 Renamed routes to have sender/receiver prefixes to support 2 way sync

--- receiver-app/src/main/java/org/openmrs/eip/app/db/sync/receiver/config/ReceiverConfig.java
+++ receiver-app/src/main/java/org/openmrs/eip/app/db/sync/receiver/config/ReceiverConfig.java
@@ -30,7 +30,6 @@ public class ReceiverConfig {
 	public PropertySource getReceiverPropertySource(ConfigurableEnvironment env) {
 		Map<String, Object> props = new HashMap();
 		props.put(Constants.PROP_PACKAGES_TO_SCAN, "org.openmrs.eip.app.db.sync.receiver.management.entity");
-		props.put("message.destination", "receiver-db-sync");
 		PropertySource customPropSource = new MapPropertySource("receiverPropSource", props);
 		env.getPropertySources().addLast(customPropSource);
 		
