f6e8984427825184d1a34a0a50c588f7d8a8cb61
wluyima <wyclif@openmrs.org>
2021-07-26 22:34:16 -0500

    249575: PrometheusFilter should only be registered if prometheus is enabled and exposed

--- receiver-app/src/main/java/org/openmrs/eip/app/db/sync/receiver/config/ReceiverPrometheusConfig.java
+++ receiver-app/src/main/java/org/openmrs/eip/app/db/sync/receiver/config/ReceiverPrometheusConfig.java
@@ -4,15 +4,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.DispatcherType;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.openmrs.eip.app.db.sync.receiver.PrometheusFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnAvailableEndpoint;
+import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ReceiverPrometheusConfig {
@@ -20,10 +21,10 @@ public class ReceiverPrometheusConfig {
 	private static final String METER_PREFIX = "openmrs_dbsync_receiver_";
 	
 	@Bean
-	public FilterRegistrationBean<PrometheusFilter> getPrometheusMetricsConfig(@Autowired MeterRegistry meterRegistry,
-	                                                                           @Autowired ProducerTemplate producerTemplate) {
+	@ConditionalOnAvailableEndpoint(endpoint = PrometheusScrapeEndpoint.class)
+	public FilterRegistrationBean<PrometheusFilter> registerPrometheusFilter(@Autowired MeterRegistry meterRegistry,
+	                                                                         @Autowired ProducerTemplate producerTemplate) {
 		
-		//TODO check if prometheus is enabled and exposed before adding the filter
 		AtomicInteger errorsGauge = meterRegistry.gauge(METER_PREFIX + "errors", new AtomicInteger());
 		AtomicInteger conflictsGauge = meterRegistry.gauge(METER_PREFIX + "conflicts", new AtomicInteger());
 		
