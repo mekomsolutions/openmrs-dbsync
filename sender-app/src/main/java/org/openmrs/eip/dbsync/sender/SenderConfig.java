e1d1dd0173bf5189579412c09c147b26e2f1cf45
wluyima <wyclif@openmrs.org>
2021-09-15 15:17:28 -0500

    EIP-50 - Make the list of watched table names configurable

---
+++ sender-app/src/main/java/org/openmrs/eip/dbsync/sender/SenderConfig.java
@@ -0,0 +1,8 @@
+package org.openmrs.eip.dbsync.sender;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("sender-application.properties")
+public class SenderConfig {}
