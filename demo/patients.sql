d7e4e56ad1705783fa24cdf261125ed91e4addeb
wluyima <wyclif@openmrs.org>
2021-11-09 21:57:11 -0600

    EIP-61 - Re-index all patient names and identifiers in parallel, update demo docker setup

--- demo/patients.sql
+++ demo/patients.sql
@@ -1,4 +1,6 @@
 INSERT INTO person (person_id,gender,birthdate,creator,date_created,voided,uuid) VALUES (3, 'M', '2000-01-01', 1, now(), 0, 'person-uuid');
-INSERT INTO person_name (person_id,given_name,family_name,creator,date_created,voided,uuid) VALUES (3, 'John', 'Doe', 1, now(), 0, 'name-uuid');
+INSERT INTO person_name (person_id,given_name,family_name,preferred,creator,date_created,voided,uuid) VALUES (3, 'John', 'Doe', 1, 1, now(), 0, 'name-uuid');
+INSERT INTO person_name (person_id,given_name,family_name,creator,date_created,voided,uuid) VALUES (3, 'Horatio', 'Hornblower', 1, now(), 0, 'name-uuid-2');
 INSERT INTO patient (patient_id,creator,date_created) VALUES (3, 1, now());
-INSERT INTO patient_identifier (patient_id,identifier,identifier_type,location_id,creator,date_created,voided,uuid) VALUES (3, '12345', 2, 1, 1, now(), 0, 'id-uuid');
+INSERT INTO patient_identifier (patient_id,identifier,identifier_type,preferred,location_id,creator,date_created,voided,uuid) VALUES (3, '12345', 2, 1, 1, 1, now(), 0, 'id-uuid');
+INSERT INTO patient_identifier (patient_id,identifier,identifier_type,location_id,creator,date_created,voided,uuid) VALUES (3, '98765', 1, 1, 1, now(), 0, 'id-uuid-2');
