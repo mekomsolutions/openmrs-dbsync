0dbfb0be2718776dd1e19497d3dbe77bcd44e143
wluyima <wyclif@openmrs.org>
2021-11-18 20:23:58 -0600

    EIP-61 - Partial implementation of PersonSenderTest

--- sender-app/src/test/java/org/openmrs/eip/dbsync/sender/PersonSenderTest.java
+++ sender-app/src/test/java/org/openmrs/eip/dbsync/sender/PersonSenderTest.java
@@ -1,11 +1,20 @@
 package org.openmrs.eip.dbsync.sender;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 import org.junit.Test;
 
 public class PersonSenderTest extends BaseSenderTest {
 	
 	@Test
-	public void shouldProcessAPersonInsertEvent() {
+	public void shouldProcessAPersonInsertEvent() throws Exception {
+		producerTemplate.sendBody("openmrs-watcher:odoo", null);
+		try (Connection c = openmrsDataSource.getConnection(); Statement s = c.createStatement()) {
+			s.executeUpdate(
+			    "INSERT INTO person (gender,creator,date_created,birthdate,birthtime,birthdate_estimated,dead,voided,deathdate_estimated,uuid) "
+			            + "VALUES('M', 1, now(), now(), now(), 0, 0, 0, 0,'test-uuid')");
+		}
 		
 	}
 	
