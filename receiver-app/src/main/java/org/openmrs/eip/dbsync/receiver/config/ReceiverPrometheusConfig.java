1d6cc304a54592482c406520ee841f5d31b21471
wluyima <wyclif@openmrs.org>
2022-04-29 17:46:13 -0500

    TFS-309730 DbSync add a gauge to monitor the number of messages to process and to retry

--- receiver-app/src/main/java/org/openmrs/eip/dbsync/receiver/config/ReceiverPrometheusConfig.java
+++ receiver-app/src/main/java/org/openmrs/eip/dbsync/receiver/config/ReceiverPrometheusConfig.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import org.apache.camel.ProducerTemplate;
 import org.openmrs.eip.dbsync.receiver.management.entity.ConflictQueueItem;
 import org.openmrs.eip.dbsync.receiver.management.entity.ReceiverRetryQueueItem;
+import org.openmrs.eip.dbsync.receiver.management.entity.SyncMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.health.Status;
@@ -27,6 +28,17 @@ public class ReceiverPrometheusConfig {
 	@Bean("openmrsDsHealthIndicator")
 	public DataSourceHealthIndicator getDataSourceHealthIndicator(@Autowired @Qualifier(OPENMRS_DATASOURCE_NAME) DataSource dataSource) {
 		return new DataSourceHealthIndicator(dataSource);
+	}
+	
+	@Bean("receiverSyncMsgMeter")
+	public MeterBinder getSyncMessagesMeterBinder(@Autowired ProducerTemplate producerTemplate) {
+		
+		return (registry) -> Gauge.builder(METER_PREFIX + "sync_messages", () -> {
+			String entity = SyncMessage.class.getName();
+			return producerTemplate.requestBody("jpa:" + entity + "?query=SELECT count(*) FROM " + entity, null,
+			    Integer.class);
+		}).register(registry);
+		
 	}
 	
 	@Bean("receiverErrorsMeter")
