23e2a76c7957268897d0da1cde851c415ea13c84
wluyima <wyclif@openmrs.org>
2022-06-08 15:07:37 +0300

    EIP-110 - Removed unused code in SyncEntityRepository and tests

--- api/src/main/java/org/openmrs/eip/dbsync/repository/SyncEntityRepository.java
+++ api/src/main/java/org/openmrs/eip/dbsync/repository/SyncEntityRepository.java
@@ -1,21 +1,7 @@
 package org.openmrs.eip.dbsync.repository;
 
-import java.util.List;
-
 import org.openmrs.eip.dbsync.entity.BaseEntity;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface SyncEntityRepository<E extends BaseEntity> extends OpenmrsRepository<E> {
-	
-	/**
-	 * Gets a page of entities with entities that have an id less than the specified value
-	 * 
-	 * @param id the maximum id to match
-	 * @param pageable {@link Pageable} object
-	 * @return A List of results
-	 */
-	List<E> findByIdLessThanEqual(Long id, Pageable pageable);
-	
-}
+public interface SyncEntityRepository<E extends BaseEntity> extends OpenmrsRepository<E> {}
