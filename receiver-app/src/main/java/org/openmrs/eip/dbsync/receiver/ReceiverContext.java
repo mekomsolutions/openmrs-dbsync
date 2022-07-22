a8a4fac69ef904d7fa8658a82a38b3ed7e4bc88b
wluyima <wyclif@openmrs.org>
2022-07-22 13:06:50 -0400

    EIP-93 - Added initial code to track last sync date for each site

--- receiver-app/src/main/java/org/openmrs/eip/dbsync/receiver/ReceiverContext.java
+++ receiver-app/src/main/java/org/openmrs/eip/dbsync/receiver/ReceiverContext.java
@@ -1,5 +1,12 @@
 package org.openmrs.eip.dbsync.receiver;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.camel.ProducerTemplate;
+import org.openmrs.eip.dbsync.SyncContext;
+import org.openmrs.eip.dbsync.receiver.management.entity.SiteInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +25,8 @@ public final class ReceiverContext {
 	
 	private static boolean isStopping = false;
 	
+	private static Map<String, SiteInfo> siteNameAndInfoMap = null;
+	
 	/**
 	 * Turn on a flag which is monitored by sync message consumers to allow them to gracefully stop
 	 * message consumption and processing before the application comes to a stop.
@@ -34,6 +43,38 @@ public final class ReceiverContext {
 	 */
 	public static boolean isStopSignalReceived() {
 		return isStopping;
+	}
+	
+	private static Map<String, SiteInfo> getSiteNameAndInfoMap() {
+		synchronized (ReceiverContext.class) {
+			if (siteNameAndInfoMap == null) {
+				log.info("Loading sites...");
+				
+				ProducerTemplate producerTemplate = SyncContext.getBean(ProducerTemplate.class);
+				String siteClass = SiteInfo.class.getSimpleName();
+				final String siteQueryUri = "jpa:" + siteClass + " ?query=SELECT s FROM " + siteClass + " s";
+				List<SiteInfo> sites = producerTemplate.requestBody(siteQueryUri, null, List.class);
+				siteNameAndInfoMap = new HashMap(sites.size());
+				sites.stream().forEach((site) -> siteNameAndInfoMap.put(site.getIdentifier().toLowerCase(), site));
+				
+				if (log.isDebugEnabled()) {
+					log.debug("Loaded sites: " + sites);
+				}
+				
+				log.info("Successfully loaded " + sites.size() + " site(s)");
+			}
+		}
+		
+		return siteNameAndInfoMap;
+	}
+	
+	/**
+	 * Gets {@link SiteInfo} that matches the specified identifier
+	 *
+	 * @return {@link SiteInfo} object
+	 */
+	public static SiteInfo getSiteInfo(String identifier) {
+		return getSiteNameAndInfoMap().get(identifier.toLowerCase());
 	}
 	
 }
