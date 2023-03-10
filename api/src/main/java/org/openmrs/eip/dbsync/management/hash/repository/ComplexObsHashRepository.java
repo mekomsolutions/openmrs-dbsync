fd28d0ed59d8985c85cf888a3f9bcb7a4777ef59
wluyima <wyclif@openmrs.org>
2023-03-10 18:12:46 +0300

    EIP-126 - Added missing ComplexObsHashRepository

---
+++ api/src/main/java/org/openmrs/eip/dbsync/management/hash/repository/ComplexObsHashRepository.java
@@ -0,0 +1,5 @@
+package org.openmrs.eip.dbsync.management.hash.repository;
+
+import org.openmrs.eip.dbsync.management.hash.entity.ComplexObsHash;
+
+public interface ComplexObsHashRepository extends BaseHashRepository<ComplexObsHash> {}
